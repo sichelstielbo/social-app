ca894253f27bfdeb5640beb7d07b9fb86c1feade
sichelstielbo <129999134+sichelstielbo@users.noreply.github.com>
2023-11-14 19:20:35 -0500

    Commit 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import AuthProvider from './context/AuthContext.tsx'
+import { QueryProvider } from './lib/react-query/QueryProvider.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <QueryProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryProvider>
+  </BrowserRouter>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['class'],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
     container: {
@@ -58,5 +60,4 @@ export default {
     },
   },
   plugins: [import('tailwindcss-animate')],
-}
-
+};
